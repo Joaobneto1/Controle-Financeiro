e6b131cdb8542aacfc7faa0f033149d7c8c07159
Joao Batista <126587508+Joaobneto1@users.noreply.github.com>
2024-08-13 21:09:28 -0300

    routes add path

--- dindin/src/routes.tsx
+++ dindin/src/routes.tsx
@@ -7,6 +7,7 @@ export const RoutesApp = () => {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route element={<PrivateRoute />}>
